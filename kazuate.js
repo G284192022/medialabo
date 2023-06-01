f300c05bf99e0f9aa2af9051cf78fe78a63ef855
G284192022 <228419@st.takushoku-u.ac.jp>
2023-06-01 14:02:47 +0900

    変更

--- kazuate.js
+++ kazuate.js
@@ -6,9 +6,6 @@ console.log("答え（デバッグ用）:" + kotae);
 // 入力回数（予想回数）
 let kaisu = 0;
 
-// 予想を4回実行する
-// 将来以下の hantei(); の4回の呼び出しを全て削除する
-// 代わりにここでは，ボタンを押したら hantei() を呼び出すイベント処理をする
 let b1 = document.querySelector('button#print');
 b1.addEventListener('click', hantei);
 
